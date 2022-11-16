e71e4a4563014cbeeb70fe2984368aa7340aef6c
olga-zak <olgaliam@gmail.com>
2022-11-16 22:21:45 +0200

    add Profile.propTypes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
-import user from '../user.json';
+
 import { Profile } from './Profile/Profile';
+import user from '../data/user.json';
 
 export const App = () => {
   return (
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,14 +1,12 @@
+import PropTypes from 'prop-types';
+
 export const Profile = ({ username, tag, location, avatar, stats }) => {
-   // const { username, tag, location, avatar, stats } = props; //деструктуризация
+  // const { username, tag, location, avatar, stats } = props; //деструктуризация
   return (
     <>
       <div className="profile">
         <div className="description">
-          <img
-            src={avatar}
-            alt="User avatar"
-            className="avatar"
-          />
+          <img src={avatar} alt="User avatar" className="avatar" />
           <p className="name">{username}</p>
           <p className="tag">{tag}</p>
           <p className="location">{location}</p>
@@ -31,4 +29,17 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
       </div>
     </>
   );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string,
+  // stats: PropTypes.object,
+  stats: PropTypes.shape({
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+  }),
 };
