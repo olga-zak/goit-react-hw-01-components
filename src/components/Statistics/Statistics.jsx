636177b15ebe156f964a6605fc127981769bf88b
olga-zak <olgaliam@gmail.com>
2022-11-19 23:44:18 +0200

    add styles for Statistics comp

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,16 +1,25 @@
 import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
 
 export const Statistics = ({ stats, title }) => {
   return (
     <>
-      <section className="statistics">
-        {title && <h2 className="title">{title}</h2>}
-        <ul className="stat-list">
+      <section className={s.statistics}>
+        {title && <h2 className={s.title}>{title}</h2>}
+        <ul className={s.statList}>
           {stats.map(({ id, label, percentage }) => {
             return (
-              <li className="item" key={id}>
-                <span className="label">{label}</span>
-                <span className="percentage">{percentage}%</span>
+              <li
+                className={s.item}
+                key={id}
+                style={{
+                  backgroundColor: `#${Math.floor(
+                    Math.random() * 16777215
+                  ).toString(16)}`,
+                }}
+              >
+                <span className={s.label}>{label}</span>
+                <span className={s.percentage}>{percentage}%</span>
               </li>
             );
           })}
@@ -28,6 +37,6 @@ Statistics.propTypes = {
       label: PropTypes.string.isRequired,
       percentage: PropTypes.number.isRequired,
     }).isRequired
-  ).isRequired, //пропс - это массив объектов
+  ).isRequired, //пропс - это массив объектов, в каждом объекте 3 свойства
   title: PropTypes.string,
 };
